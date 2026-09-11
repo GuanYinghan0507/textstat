7e3eb81ece22a27a226616d85c0c55fc3aab4da3
hanygx <133588529+hanygx@users.noreply.github.com>
2026-09-11 23:49:45 +0800

    删除误传的脚本
    * 删除误传的脚本
    
    * 删除误传的脚本
    
    * 删除误传的脚本
    
    * 删除误传的脚本
    
    * 删除误传的脚本
    
    * 删除误传的脚本
    
    * 删除误传的脚本
    
    * 删除误传的脚本
    
    * 删除误传的脚本
    
    * 删除误传的脚本
    
    * 删除误传的脚本
    
    * 删除误传的脚本
    
    * 删除误传的脚本
    
    * 删除误传的脚本
    
    * 删除误传的脚本
    
    * 删除误传的脚本
    
    * 删除误传的脚本
    
    * 删除误传的脚本
    
    * 删除误传的脚本

--- assignment/module1/tests/metrics/test_metrics_automated_readability_index.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-from metrics_helpers import run_case
-
-
-def test_textstat_st_rm_005(record_property) -> None:
-    run_case("TEXTSTAT-ST-RM-005", record_property)
--- assignment/module1/tests/metrics/test_metrics_crawford.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-from metrics_helpers import run_case
-
-
-def test_textstat_st_rm_016(record_property) -> None:
-    run_case("TEXTSTAT-ST-RM-016", record_property)
